2e44c4d01ce45a945a3f2bc9185dfed364b03559
Devsk001 <srik3713@gmail.com>
2023-03-21 20:45:29 +0530

    Fitness Js By DevSk

---
+++ fitness.js
@@ -0,0 +1,12 @@
+const km=[4,1,3,7,10];
+// for loop for acess the km
+for(i=0;i<km.length;i++)
+{
+    const fit=(km[i]*2)*5;
+    console.log("Travel In Week : ",fit);
+}
+/*
+  Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is XXX km away from his home.
+
+If his office is open on 555 days in a week, find the number of kilometers Chef travels through office trips in a week.
+*/
